d9f9bb06e66226327454a565d9146c91ba0958c7
heiderp <heiderperdomo6@gmail.com>
2022-07-01 10:36:04 -0400

    Actulizacion de Estilos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import IpForm from "./IpForm";
 const Header = ({ ipInfo }) => {
   return (
     <div className="header">
-      <h2>IP Tracker</h2>
+      <h2 className="header-title">IP Tracker</h2>
       <IpForm />
       <Card ipInfo={ipInfo} />
     </div>
