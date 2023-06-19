47d9ba7c2a8a3e50400946eeb709b8debeaa0440
Heider Perdomo <heiderperdomo6@gmail.com>
2023-06-19 12:33:09 -0400

    refactor: context with hooks

--- src/context/IpContext.jsx
+++ src/context/IpContext.jsx
@@ -1,10 +1,18 @@
 import React, { createContext, useState } from 'react'
+import useGetIpInfo from '../hooks/useGetIpInfo'
 
 const IpProvider = ({ children }) => {
-  const [ipFound, setIpFound] = useState('8.8.8.8')
-  const updateIp = (data) => setIpFound(data)
+  const [ip, setIp] = useState(null)
+  const updateIp = (data) => setIp(data)
+  const { states, handles } = useGetIpInfo()
+
+  const globalState = {
+    states: { ...states, ip },
+    handles: { ...handles, updateIp }
+  }
+
   return (
-    <IpContext.Provider value={{ ipFound, updateIp }}>
+    <IpContext.Provider value={globalState}>
       {children}
     </IpContext.Provider>
   )
