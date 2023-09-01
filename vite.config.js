6a274279f4ad110c5770e8f50fc1781fcf2bdb89
Heider Perdomo <heiderperdomo6@gmail.com>
2023-09-01 15:53:05 -0400

    chrone: define aliases

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@:': `${__dirname}/src`,
+      '@components': `${__dirname}/src/components`,
+      '@context': `${__dirname}/src/context`,
+      '@hooks': `${__dirname}/src/hooks`,
+      '@services': `${__dirname}/src/services`,
+      '@utils': `${__dirname}/src/utils`,
+      '@styles': `${__dirname}/src/styles`,
+      '@assets': `${__dirname}/src/assets`,
+    }
+  }
 })
