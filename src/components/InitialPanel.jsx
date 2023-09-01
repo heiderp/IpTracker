0e43a09697f2bd27ff0288b146cb37d59d010063
Heider Perdomo <heiderperdomo6@gmail.com>
2023-09-01 15:56:48 -0400

    feat: InitialPanel
    - added svg

--- src/components/InitialPanel.jsx
+++ src/components/InitialPanel.jsx
@@ -1,7 +1,8 @@
+import MapIcon from '@assets/icons/map-search-svgrepo-com.svg'
 const InitialPanel = () => {
   return (
     <div className='init_panel'>
-      <img src={''} alt="no-map-seter" className="init_panel-image" />
+      <img src={MapIcon} alt="no-map-seter" className="init_panel-image" />
       <p className='init_panel-subtitle'>Preparado para su busqueda</p>
     </div>
   )
